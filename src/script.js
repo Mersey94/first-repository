d052fd0ab70bd5d8a8afea0034ef37927ff777c5
Mersey <54132987+Mersey94@users.noreply.github.com>
2021-09-16 16:56:17 +1000

    updates JS with live 5 day forecast data/api

--- src/script.js
+++ src/script.js
@@ -1,59 +1,59 @@
-let weatherIcons = {
-  200: "fa-thunderstorm",
-  201: "fa-thunderstorm",
-  202: "fa-thunderstorm",
-  211: "fa-thunderstorm",
-  212: "fa-thunderstorm",
-  221: "fa-thunderstorm",
-  230: "fa-thunderstorm",
-  231: "fa-thunderstorm",
-  232: "fa-thunderstorm",
-  300: "fa-thunderstorm",
-  301: "fa-cloud-drizzle",
-  302: "fa-cloud-drizzle",
-  310: "fa-cloud-drizzle",
-  311: "fa-cloud-drizzle",
-  312: "fa-cloud-drizzle",
-  313: "fa-cloud-drizzle",
-  314: "fa-cloud-drizzle",
-  321: "fa-cloud-drizzle",
-  500: "fa-cloud-drizzle",
-  501: "fa-cloud-showers",
-  502: "fa-cloud-showers-heavy",
-  503: "fa-cloud-showers-heavy",
-  504: "fa-cloud-showers-heavy",
-  511: "fa-cloud-showers",
-  520: "fa-cloud-showers",
-  521: "fa-cloud-showers",
-  522: "fa-cloud-showers-heavy",
-  531: "fa-cloud-showers-heavy",
-  600: "fa-cloud-snow",
-  601: "fa-cloud-snow",
-  602: "fa-snow-blowing",
-  611: "fa-cloud-sleet",
-  612: "fa-cloud-sleet",
-  613: "fa-cloud-sleet",
-  615: "fa-cloud-hail-mixed",
-  616: "fa-cloud-hail-mixed",
-  620: "fa-cloud-hail-mixed",
-  621: "fa-cloud-snow",
-  622: "fa-snow-blowing",
-  701: "fa-fog",
-  711: "fa-smoke",
-  721: "fa-sun-haze",
-  731: "fa-sun-dust",
-  741: "fa-fog",
-  751: "fa-sun-dust",
-  761: "fa-sun-dust",
-  762: "fa-volcano",
-  771: "fa-wind",
-  781: "fa-tornado",
-  800: "fa-sun",
-  801: "fa-cloud",
-  802: "fa-cloud-sun",
-  803: "fa-clouds",
-  804: "fa-clouds",
-};
+// let weatherIcons = {
+//   200: "fa-thunderstorm",
+//   201: "fa-thunderstorm",
+//   202: "fa-thunderstorm",
+//   211: "fa-thunderstorm",
+//   212: "fa-thunderstorm",
+//   221: "fa-thunderstorm",
+//   230: "fa-thunderstorm",
+//   231: "fa-thunderstorm",
+//   232: "fa-thunderstorm",
+//   300: "fa-thunderstorm",
+//   301: "fa-cloud-drizzle",
+//   302: "fa-cloud-drizzle",
+//   310: "fa-cloud-drizzle",
+//   311: "fa-cloud-drizzle",
+//   312: "fa-cloud-drizzle",
+//   313: "fa-cloud-drizzle",
+//   314: "fa-cloud-drizzle",
+//   321: "fa-cloud-drizzle",
+//   500: "fa-cloud-drizzle",
+//   501: "fa-cloud-showers",
+//   502: "fa-cloud-showers-heavy",
+//   503: "fa-cloud-showers-heavy",
+//   504: "fa-cloud-showers-heavy",
+//   511: "fa-cloud-showers",
+//   520: "fa-cloud-showers",
+//   521: "fa-cloud-showers",
+//   522: "fa-cloud-showers-heavy",
+//   531: "fa-cloud-showers-heavy",
+//   600: "fa-cloud-snow",
+//   601: "fa-cloud-snow",
+//   602: "fa-snow-blowing",
+//   611: "fa-cloud-sleet",
+//   612: "fa-cloud-sleet",
+//   613: "fa-cloud-sleet",
+//   615: "fa-cloud-hail-mixed",
+//   616: "fa-cloud-hail-mixed",
+//   620: "fa-cloud-hail-mixed",
+//   621: "fa-cloud-snow",
+//   622: "fa-snow-blowing",
+//   701: "fa-fog",
+//   711: "fa-smoke",
+//   721: "fa-sun-haze",
+//   731: "fa-sun-dust",
+//   741: "fa-fog",
+//   751: "fa-sun-dust",
+//   761: "fa-sun-dust",
+//   762: "fa-volcano",
+//   771: "fa-wind",
+//   781: "fa-tornado",
+//   800: "fa-sun",
+//   801: "fa-cloud",
+//   802: "fa-cloud-sun",
+//   803: "fa-clouds",
+//   804: "fa-clouds",
+// };
 //current location & weather,
 let key = "bc2cd97eaa209e7d22d8f3c84081655f";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${key}&`;
@@ -68,7 +68,6 @@ let cityInput = document.querySelector("#city-input");
 let feelsLike = document.querySelector(".temp-feel");
 let tempHumid = document.querySelector(".li-humid");
 let tempWind = document.querySelector(".li-wind");
-let iconElement = document.querySelector("#icon");
 
 document.querySelector("#search-btn").addEventListener("click", function () {
   event.preventDefault();
@@ -88,6 +87,8 @@ document
 document.querySelector("#celsius-link").addEventListener("click", function () {
   displayTemp(temp, true);
 });
+
+document.querySelector("#weatherIcon");
 //functions
 
 function citySearched(search) {
@@ -105,15 +106,15 @@ function showPosition(position) {
   let lon = position.coords.longitude;
   let url = `${apiUrl}lat=${lat}&lon=${lon}`;
   axios.get(url).then(displayWeather);
+
+  getForecast(response.data.coord);
+  // console.log(position);
 }
 
 function displayWeather(response) {
-  console.log(response.data.main.temp_max);
-  let minTemp = response.data.main.temp_min;
-  let maxTemp = response.data.main.temp_max;
-
+  let coordinates = response.data.coord;
   let iconID = response.data.weather[0].id;
-  console.log(weatherIcons[iconID]);
+  //console.log(weatherIcons[iconID]);
   let humid = response.data.main.humidity;
   let wind = response.data.wind.speed;
   let currentDate = new Date(response.data.dt * 1000);
@@ -133,29 +134,23 @@ function displayWeather(response) {
   document.querySelector(".li-humid").innerHTML = humid;
   document.querySelector(".li-wind").innerHTML = wind;
 
+  // document
+  //   .querySelector("#icon")
+  //   .setAttribute("class", `fal current-icon ${weatherIcons[iconID]}`);
+  console.log(response.data.weather[0].icon);
   document
     .querySelector("#icon")
-    .setAttribute("class", `fal current-icon ${weatherIcons[iconID]}`);
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
   feelsLike.innerHTML = `${Math.round(feels)}°C`;
   tempHumid.innerHTML = `${humid} %`;
   tempWind.innerHTML = `${wind} km/h`;
-  d1MinTemp.innerHTML = `${Math.round(feels)}°C`;
-  d1MaxTemp.innerHTML = `${Math.round(feels)}°C`;
-
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${iconID}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
 
   showDate(new Date());
+  getForecast(coordinates);
 }
-// function displayForecast(response) {
-//   console.log(response.data);
-
-//   let forecast = response.data.daily;
-//   let forecastElement = document.querySelector(".col .day");
-// }
 
 function displayTemp(temperature, isCelsius) {
   let unit = "";
@@ -171,7 +166,7 @@ function displayTemp(temperature, isCelsius) {
 function showDate(date) {
   let offset = date.getTimezoneOffset() / -60;
   date.setTime(date.getTime() - offset * 3600 * 1000 + timezone * 1000);
-  console.log(offset);
+  // console.log(offset);
   let days = [
     "Sunday",
     "Monday",
@@ -214,3 +209,50 @@ setInterval(function () {
   showDate(new Date());
 }, 1000);
 citySearched("Sydney");
+
+// forecast js
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  date.setTime(date.getTime() + timezone * 1000);
+  console.log(date);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  console.log(day);
+  return days[day];
+}
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  // console.log(response.data.timezone_offset);
+  forecast.forEach(function (forecastDay, index) {
+    // console.log(forecastDay.dt);
+    let iconID = forecastDay.weather[0].id;
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col preview-day">
+          <div id="d1Day"class="col day day1">${formatDay(forecastDay.dt)}</div>
+          <div><div id="forecastIcon">
+            <img class="icon" src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png" />
+          </div></div>
+          <span id="forecastMaxTemp"class="col day-max-temp">${Math.round(
+            forecastDay.temp.max
+          )}° |</span>
+          <span id="forecastMinTemp"class="col day-min-temp"><i class="fal fa-moon"></i>${Math.round(
+            forecastDay.temp.min
+          )}°</span>
+        </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
